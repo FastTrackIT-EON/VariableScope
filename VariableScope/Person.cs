f7b659467496258b44a4b0ba4d284336f6a1d64c
Florin Lazar <florin.lazar.it2014@gmail.com>
2024-04-22 14:48:08 +0300

    push changes

---
+++ VariableScope/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableScope
+{
+    internal class Person
+    {
+        string name = "john doe";
+        int age;
+
+        void SayHello2()
+        {
+            string name = "other";
+            Console.WriteLine("Hello, my name is " + name);
+        }
+
+        void SayHello()
+        {
+            Console.WriteLine("Hello, my name is " + name);
+        }
+
+        
+
+        
+    }
+}
